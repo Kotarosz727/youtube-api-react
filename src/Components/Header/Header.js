9e9616570fbe939859e1dbe24ce52f92d9eef5a1
鈴木光太郎 <suzukikoutarou@suzukikoutarounoMacBook-Pro.local>
2020-10-22 12:53:41 +0900

    add fontawesome and sass

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import Style from "./Header.module.scss"
 
 const Header = () => {
-    return (
-        <div>
-            Header
-        </div>
-    )
-}
+  return (
+    <div className={Style.header}>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+      <div>
+        <form>
+          <input type="text" placeholder="serach"></input>
+          <button type="submit"><FontAwesomeIcon icon={faSearch} /></button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
